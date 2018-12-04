ab5ccbbf8c3d8e992f814928434a00aa5318c14b
Mahendra Shinde <MahendraShinde@synergetics-india.com>
2018-12-04 17:03:11 +0530

    Updates 2

--- Dockerfile
+++ Dockerfile
@@ -8,4 +8,6 @@ RUN mkdir ./webapps
 
 COPY ./DeepSpace/target/deepspace.war /usr/local/tomcat/webapps/ROOT.war
 
+EXPOSE 8080
+
 CMD ["catalina.sh","run"]
--- script.sh
+++ script.sh
@@ -3,4 +3,4 @@
 ### Assuming DOCKER pre-installed in system
 docker pull mahendrshinde/deep-space:latest
 ## Launch the application
-docker run -d -p 8080:8080 mahendrshinde/deep-space:latest 
+docker run -d -p 8080:8080 mahendrshinde/deep-space:lates
